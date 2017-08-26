7dde943df0e50c6df227ab381f1d51c173eebe8f
David Czeck <david@ibiblio.org>
2017-08-26 11:51:19 -0700

    Update tools and working environment.

--- systemjs.config.js
+++ systemjs.config.js
@@ -7,7 +7,7 @@
 
   // map tells the System loader where to look for things
   var map = {
-    'app':                        'app', // 'dist',
+    'app':                        'runt', // 'dist',
     'rxjs':                       'node_modules/rxjs',
     '@angular':                   'node_modules/@angular'
   };
